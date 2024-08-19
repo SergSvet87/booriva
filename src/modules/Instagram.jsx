048f68fcc9272e6886f6b8933830db86dd5eeba1
Maxnightik <xiaomimax4n@gmail.com>
2024-08-20 01:54:55 +0300

    update blocks for mainpage

--- src/modules/Instagram.jsx
+++ src/modules/Instagram.jsx
@@ -1,5 +1,39 @@
 export const Instagram = () => {
-    return(
-        <section className="instagram"></section>
-    )
-}
+  return (
+    <section className="instagram">
+      <div className="container">
+        <div className="instagram__wrap">
+          <div className="instagram__col instagram__col-left">
+            <div className="instagram__col-title">
+              <img src="public/images/instagram-title.png" />
+              <p>Мы в экстазе, когда ты нас отмечаешь) Отмечай плз чаще</p>
+            </div>
+            <div className="instagram__col-image">
+              <img src="public/images/instagram-1.jpg" />
+              <img
+                className="instagram__col-image-2"
+                src="public/images/instagram-2.jpg"
+              />
+            </div>
+          </div>
+          <div className="instagram__col instagram__col-center">
+            <img src="public/images/instagram-3.jpg" />
+          </div>
+          <div className="instagram__col instagram__col-right">
+            <div className="instagram__col-title">
+              <p className="instagram__col-title-hashtag">#boorivagirls</p>
+              <p>Ставь тэг чтобы быть в нашей тусовке.</p>
+            </div>
+            <div className="instagram__col-image">
+              <img
+                className="instagram__col-image-4"
+                src="public/images/instagram-4.jpg"
+              />
+              <img src="public/images/instagram-5.jpg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
