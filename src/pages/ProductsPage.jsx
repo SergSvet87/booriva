8b6fee6142891420f213c30f27bc205d5375fb90
Serhii Svitlychnyi <svetlichniy1905@gmail.com>
2024-09-02 17:24:27 +0300

    додав фільтр в products-page

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,14 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
-import { BreadCrumbs } from "../modules/BreadCrumbs";
+import { BreadCrumbs } from '../modules/BreadCrumbs';
+import Filter from '../modules/products-page/Filter';
 
 export const ProductsPage = () => {
-  const { category, subcategory } = useParams();
+	const { category, subcategory } = useParams();
 
-  return (
-    <>
-      <BreadCrumbs category={category} subCategory={subcategory} />
-      <h1>ProductsPage</h1>
-    </>
-  );
+	return (
+		<>
+			<BreadCrumbs category={category} subCategory={subcategory} />
+
+			<div className="container">
+				<h1>ProductsPage</h1>
+
+				<Filter />
+			</div>
+		</>
+	);
 };
